0a5e4e7c8e108866b732df9d4d38c7df8d2d5991
prof69 <testsite69000@gmail.com>
2021-01-27 08:18:26 +0100

    Ajout d'un nouveau projet

---
+++ src/main/java/com/acar/ConnectionBDD/Connexion.java
@@ -0,0 +1,62 @@
+package com.acar.ConnectionBDD;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Connexion {
+	
+	static Connection con = null;
+	static PreparedStatement prepareStat = null;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3308/universite?serverTimezone=UTC", "root", "");
+			if (con != null) {
+				System.out.println("Connexion réussi");
+			} else {
+				System.out.println("Connexion échouée");
+			}
+			
+			String insertQueryStatement = "INSERT  INTO  etudiant  VALUES  (?,?,?,?,?,?,?)";
+			
+			prepareStat = con.prepareStatement(insertQueryStatement);
+			prepareStat.setInt(1, 128);
+			prepareStat.setString(2, "Pierre");
+			prepareStat.setString(3, "Paul");
+			prepareStat.setString(4, "1990-08-19");
+			prepareStat.setString(5, "rue de la victoire");
+			prepareStat.setString(6, "69000");
+			prepareStat.setString(7, "Lyon");
+			
+			prepareStat.executeUpdate();
+			
+			
+			String getQueryStatement = "SELECT * FROM etudiant";
+			prepareStat = con.prepareStatement(getQueryStatement);
+			ResultSet rs = prepareStat.executeQuery();
+			while (rs.next()) {
+				int numetu = rs.getInt("numetu");
+				String nom = rs.getString("nom");
+				
+				System.out.println("numetu : " + numetu + ", nom : " + nom);
+			}
+			
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			return;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+ 
+		
+
+	}
+
+}
